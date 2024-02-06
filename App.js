18773ece8d77d2f3d12e119ae16ecf7b82eca132
chrisd94 <65397716+chrisd94@users.noreply.github.com>
2024-02-06 17:50:43 +0100

    First commit

--- App.js
+++ App.js
@@ -3,18 +3,42 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container_top}>
+      <View style={styles.container_pink}>
+        <Text>Open up App.js start to working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.container_white}>
+        <Text>Open up App.js start to working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
+  
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  container_white: {
+    width: '97%',
+    flex: 2,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'center',
+    borderTopRightRadius: 50,
+  },
+  container_pink: {
+    width: '100%',
+    flex: 1,
+    backgroundColor: '#ECDEEA',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomLeftRadius: 50,
+  },
+  container_top: {
+    width: '100%',
+    flex: 1,
+    backgroundColor: '#ECDEEA',
+    alignItems: 'left',
     justifyContent: 'center',
   },
 });
